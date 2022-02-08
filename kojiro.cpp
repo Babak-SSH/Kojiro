c2c0a52852aa0b9c20e0740b1875b7cda3c4e290
Babak-SSH <babak.sefidgar@gmail.com>
2022-02-08 21:45:08 +0330

    moved all biboard functions to its own file

--- kojiro.cpp
+++ kojiro.cpp
@@ -1,7 +1,25 @@
-#include <iostream>;
+// shoudl include or not? who cares?
+//#include "stdio.h"
+//#include "cstdint"
+
+#include "bitboard.h"
 
 int main(){
-    cout << "im the greates loser...\n";
+    uint64_t bitboard = 0ull;
+
+    printf("im the greates loser...\n");
+
+    print_bitboard(bitboard);
+
+    set_bit(bitboard, e4);
+    set_bit(bitboard, d5);
+
+    print_bitboard(bitboard);
+
+    pop_bit(bitboard, d5);
+
+    print_bitboard(bitboard);
     getchar();
+    
     return 0;    
 }

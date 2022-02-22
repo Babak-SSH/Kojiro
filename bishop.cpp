b45fe3afcb8d3edad97cdf735bcb8659e49aba34
Babak-SSH <babak.sefidgar@gmail.com>
2022-02-22 20:38:22 +0330

    add mask attack funtion for bishop with relative position and fixed for loops

--- bishop.cpp
+++ bishop.cpp
@@ -9,8 +9,8 @@
 
 
 /////////////////////////////////////////////////
-// masking relative position that can be attacked
-// by the specified bishop.
+// masking position that can be attacked by the 
+// specified bishop.
 /////////////////////////////////////////////////
 uint64_t mask_bishop_attacks(int pos){
     uint64_t attacks = 0ull;
@@ -19,28 +19,64 @@ uint64_t mask_bishop_attacks(int pos){
     // rank, file
     int r, f;
 
-    r = 7-(pos/8);
+    r = pos/8;
     f = pos%8;
 
     set_bit(attacks, pos);
 
     // up right
-    for(int i = r+1, j = f+1, k=1; i < 8 && j < 8; i++, j++, k++){
+    for(int i = r-1, j = f+1, k=1; i >= 0 && j < 8; i--, j++, k++){
         mask_bitboard |= (attacks >> (7*k));
     }
     // up left
-    for(int i = r, j = f, k=1; i < 8 && j > 0; i++, j--, k++){
+    for(int i = r-1, j = f-1, k=1; i >= 0 && j >= 0; i--, j--, k++){
         mask_bitboard |= (attacks >> (9*k));
     }
     // down right
-    for(int i = r, j = f, k=1; i > 0 && j < 8; i--, j++, k++){
+    for(int i = r+1, j = f+1, k=1; i < 8 && j < 8; i++, j++, k++){
         mask_bitboard |= (attacks << (9*k));
     }
     // down left
-    for(int i = r, j = f, k=1; i > 0 && j > 0; i--, j--, k++){
+    for(int i = r+1, j = f-1, k=1; i < 8 && j >= 0; i++, j--, k++){
         mask_bitboard |= (attacks << (7*k));
     }
     
+    return mask_bitboard;
+}
 
+/////////////////////////////////////////////////
+// masking relative position that can be attacked
+// by the specified bishop.
+// ?change this shity function name
+/////////////////////////////////////////////////
+uint64_t mask_bishop_attacks_relative(int pos){
+    uint64_t attacks = 0ull;
+    uint64_t mask_bitboard = 0ull;
+
+    // rank, file
+    int r, f;
+
+    r = pos/8;
+    f = pos%8;
+
+    set_bit(attacks, pos);
+
+    // up right
+    for(int i = r-1, j = f+1, k=1; i > 0 && j < 7; i--, j++, k++){
+        mask_bitboard |= (attacks >> (7*k));
+    }
+    // up left
+    for(int i = r-1, j = f-1, k=1; i > 0 && j > 0; i--, j--, k++){
+        mask_bitboard |= (attacks >> (9*k));
+    }
+    // down right
+    for(int i = r+1, j = f+1, k=1; i < 7 && j < 7; i++, j++, k++){
+        mask_bitboard |= (attacks << (9*k));
+    }
+    // down left
+    for(int i = r+1, j = f-1, k=1; i < 7 && j > 0; i++, j--, k++){
+        mask_bitboard |= (attacks << (7*k));
+    }
+    
     return mask_bitboard;
 }
--- bishop.h
+++ bishop.h
@@ -4,5 +4,6 @@
 #include "bitboard.h"
 
 uint64_t mask_bishop_attacks(int pos);
+uint64_t mask_bishop_attacks_relative(int pos);
 
 #endif

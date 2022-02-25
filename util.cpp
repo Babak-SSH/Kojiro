faa0aadb5e1a6b5ca95b03f25b2807d8cfefb1b8
Babak-SSH <babak.sefidgar@gmail.com>
2022-02-25 22:16:15 +0330

    add count_bits function to count number of bits in a bitboard(ull)

---
+++ util.cpp
@@ -0,0 +1,16 @@
+#include "util.h"
+
+/*there is a hardware bit count for gcc which requieres cpu type with march arg in command 
+    more analyse required to compare custome bit count and built in bit count performance :)*/
+//#define count_bits(bitboard) __builtin_popcountll(bitboard)
+
+// counts bits in a bitboard
+int count_bits(uint64_t bitboard){
+    int count = 0;
+    while(bitboard){
+        bitboard &= bitboard - 1;
+        count++;
+    }
+    
+    return count;
+}
---
+++ util.h
@@ -0,0 +1,9 @@
+#ifndef UTIL_H
+#define TUIL_H
+
+#include "cons.h" 
+#include "bitboard.h"
+
+int count_bits(uint64_t bitboard);
+
+#endif

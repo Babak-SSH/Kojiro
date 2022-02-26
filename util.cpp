fe382eaeb2d79fac9dd22839971ad104172e66d3
Babak-SSH <babak.sefidgar@gmail.com>
2022-02-26 18:59:21 +0330

    add bit count function

--- util.cpp
+++ util.cpp
@@ -1,7 +1,8 @@
 #include "util.h"
 
 /*there is a hardware bit count for gcc which requieres cpu type with march arg in command 
-    more analyse required to compare custome bit count and built in bit count performance :)*/
+    more analyse required to compare custome bit count and built in bit count performance :)
+    cool link: http://graphics.stanford.edu/~seander/bithacks.html#CountBitsSetTable*/
 //#define count_bits(bitboard) __builtin_popcountll(bitboard)
 
 // counts bits in a bitboard

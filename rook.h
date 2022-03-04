aa02b19b3ef65adfa6c37c51128f45c9eb0bde9d
Babak-SSH <babak.sefidgar@gmail.com>
2022-03-04 19:21:56 +0330

    fixed function decleration

--- rook.h
+++ rook.h
@@ -3,7 +3,8 @@
 
 #include "bitboard.h"
 
-uint64_t generate_rook_attacks_on_fly(int pos);
+uint64_t generate_rook_attacks_on_fly(int pos, uint64_t block);
+
 uint64_t mask_rook_attacks_relative(int pos);
 
 #endif

1725dd4ee9c7823e06beed6aa8bb7eb92a170931
Babak-SSH <babak.sefidgar@gmail.com>
2022-03-05 14:15:42 +0330

    fix include cycle, moved countbit and ls1bindex from util to bitboard

--- bitboard.cpp
+++ bitboard.cpp
@@ -23,6 +23,7 @@ void print_bitboard(uint64_t  bitboard){
         printf("\n");
     }
     printf("   a  b  c  d  e  f  g  h\n\n");
+    printf("BITBOARD: %llud\n\n", bitboard);
 }
 
 /////////////////////////////////////////////////
@@ -44,6 +45,45 @@ int get_bit(uint64_t bitboard, int pos){
 /////////////////////////////////////////////////
 void pop_bit(uint64_t &bitboard, int pos){
     bitboard ^= (1ull << pos);
+}
+
+/*there is a hardware bit count for gcc which requieres cpu type with march arg in command 
+    more analyse required to compare custome bit count and built in bit count performance :)
+    cool link: http://graphics.stanford.edu/~seander/bithacks.html#CountBitsSetTable*/
+//#define count_bits(bitboard) __builtin_popcountll(bitboard)
+/////////////////////////////////////////////////
+// counts bits in a bitboard by removing each 1 bit 
+// in each loop until the bitboard becomes 0.
+/////////////////////////////////////////////////
+int count_bits(uint64_t bitboard){
+    int count = 0;
+    while(bitboard){
+        bitboard &= bitboard - 1;
+        count++;
+    }
+    
+    return count;
+}
+
+/////////////////////////////////////////////////
+// gets the index of the least significant 1st bit
+// in the bitboard. (bb & -bb) will remove all 1s 
+// except lsb 1 and by summing with -1 we will convert
+// all zeros behind the lsb to 1 and count them to find
+// the index.
+/////////////////////////////////////////////////
+int get_ls1b_index(uint64_t bitboard){
+    int idx;
+    if(bitboard){
+        /* should try shifting too*/
+        idx = count_bits((bitboard & -bitboard)-1);
+        // printing square name
+        // printf("%s", convert_to_square[idx]);
+        return idx;
+    }
+    // if bitboard is invalid
+    else
+        return -1;
 }
 
 /////////////////////////////////////////////////
--- bitboard.h
+++ bitboard.h
@@ -2,8 +2,7 @@
 #define BITBOARD_H
 
 #include <cstdint>
-#include "stdio.h"
-#include "util.h"
+#include <stdio.h>
 
 // names representing each square in chess board 
 // according to their file and rank
@@ -16,10 +15,16 @@ enum square {a8,b8,c8,d8,e8,f8,g8,h8,
              a2,b2,c2,d2,e2,f2,g2,h2,
              a1,b1,c1,d1,e1,f1,g1,h1};
 
+enum color {white, black};
+
+enum piece {king, queen, rook, bishop, knight, pawn};
+
 void print_bitboard(uint64_t bitboard);
 void set_bit(uint64_t &bitboard, int pos);
 int get_bit(uint64_t bitboard, int pos);
 void pop_bit(uint64_t &bitboard, int pos);
+int count_bits(uint64_t bitboard);
+int get_ls1b_index(uint64_t bitboard);
 uint64_t set_occupancy(int index, int mask_bit_count, uint64_t attack_mask);
 
 #endif

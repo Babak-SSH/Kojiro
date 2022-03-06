be3de55368cdf8ce9034867f3826d566aedbabbc
Babak-SSH <babak.sefidgar@gmail.com>
2022-03-06 21:16:10 +0330

    add piece data types and castling rights as a binary encoding

--- types.h
+++ types.h
@@ -10,10 +10,28 @@ enum square {a8,b8,c8,d8,e8,f8,g8,h8,
              a4,b4,c4,d4,e4,f4,g4,h4,
              a3,b3,c3,d3,e3,f3,g3,h3,
              a2,b2,c2,d2,e2,f2,g2,h2,
-             a1,b1,c1,d1,e1,f1,g1,h1};
+             a1,b1,c1,d1,e1,f1,g1,h1,no_sq};
 
-enum color {white, black};
+enum Color {WHITE, BLACK, NO_COLOR};
 
-enum piece {king, queen, rook, bishop, knight, pawn};
+enum PieceType {KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN};
+
+enum Piece {W_PAWN, W_KNIGHT, W_BISHOP, W_ROOK, W_QUEEN, W_KING,
+            B_PAWN, B_KNIGHT, B_BISHOP, B_ROOK, B_QUEEN, B_KING};
+
+
+/* castling rights binary encoding
+    bin  dec
+    
+   0001    1  white king can castle to the king side
+   0010    2  white king can castle to the queen side
+   0100    4  black king can castle to the king side
+   1000    8  black king can castle to the queen side
+   examples
+   1111       both sides an castle both directions
+   1001       black king => queen side
+              white king => king side
+*/
+enum CastlingRight {WK=1, WQ=2, BK=4, BQ=8};
 
 #endif

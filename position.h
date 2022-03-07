bd70159ff0ea7a8227d665c2e998a33846d439e7
Babak-SSH <babak.sefidgar@gmail.com>
2022-03-07 18:46:12 +0330

    add set_piece function to assign the bit in all relative bitboards

--- position.h
+++ position.h
@@ -1,7 +1,8 @@
 #ifndef POSITION_H
 #define POSITION_H
 
-#include <string.h>
+#include <string>
+#include <cstring>
 
 #include "cons.h"
 #include "bitboard.h"
@@ -9,7 +10,7 @@
 
 
 // ASCII pieces
-extern const char ascii_pieces[13];
+extern const std::string ascii_pieces;
 
 // piece bitboards
 extern uint64_t bitboards[12];
@@ -33,5 +34,14 @@ void init_start();
 std::string get_fen();
 
 void parse_fen(const std::string& fen);
+
+void set_piece(Piece p, int sq);
+
+inline void set_piece(Piece p, int sq){
+    set_bit(bitboards[p], sq);
+    // getting the color of the piece whit shifting (0-7 w, 7-11 b)
+    occupancies[sq >> 3] |= bitboards[p];
+    occupancies[2]  |= bitboards[p];
+}
 
 #endif

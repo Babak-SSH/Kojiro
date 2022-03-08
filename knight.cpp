a4205ab64273dc329e914423023247ec9326f659
Babak-SSH <babak.sefidgar@gmail.com>
2022-03-08 16:25:06 +0330

    add init knight attacks function to fill the knight attacks on all 64 square and add doxy style comments

--- knight.cpp
+++ knight.cpp
@@ -8,15 +8,15 @@
 #include "knight.h"
 
 
-/////////////////////////////////////////////////
-// masking position that can be attacked by the 
-// specified knight.
-/////////////////////////////////////////////////
-uint64_t mask_knight_attacks(int pos){
+uint64_t knight_attacks[64];
+
+/// masking squares that can be attacked by the 
+/// specified knight.
+uint64_t mask_knight_attacks(int square){
     uint64_t attacks = 0ull;
     uint64_t mask_bitboard = 0ull;
 
-    set_bit(attacks, pos);
+    set_bit(attacks, square);
 
     // up
     mask_bitboard |= (attacks >> 15) & not_A_file;
@@ -32,4 +32,11 @@ uint64_t mask_knight_attacks(int pos){
     mask_bitboard |= (attacks >> 10) & not_GH_file;
 
     return mask_bitboard;
+}
+
+/// initialize knight attacks for all 64 squares.
+void init_knight_attacks(){
+    for (int sq = 0; sq < 64; sq++){
+        knight_attacks[sq] = mask_knight_attacks(sq);
+    }
 }
--- knight.h
+++ knight.h
@@ -5,8 +5,18 @@
 #include "bitboard.h"
 #include "types.h"
 
-// enum color {white, black};
+/**
+ * @brief masking squares that can be attacked by the specified knight.
+ * 
+ * @param square 
+ * @return uint64_t 
+ */
+uint64_t mask_knight_attacks(int square);
 
-uint64_t mask_knight_attacks(int pos);
+/**
+ * @brief initialize king attacks for all 64 squares.
+ * 
+ */
+void init_knight_attacks();
 
 #endif

e349d20fe3e453d2bbcee4ce4e84c7698d3ff0c8
Babak-SSH <babak.sefidgar@gmail.com>
2022-03-08 13:31:20 +0330

    add init pawn attacks function to fille the pawn attacks on all 64 square and add doxy style comments

--- pawn.cpp
+++ pawn.cpp
@@ -8,17 +8,19 @@
 #include "pawn.h"
 
 
-/////////////////////////////////////////////////
-// masking position that can be attacked by the 
-// specified pawn. this is done by shifting 
-// 7 and 9 bits according to the color and taking
-// exceptaions of A and H file to considration.
-/////////////////////////////////////////////////
-uint64_t mask_pawn_attacks(int pos, int side){
+uint64_t pawn_attacks[2][64];
+
+/// masking position that can be attacked by the 
+/// specified pawn. this is done by shifting 
+/// 7 or 9 bits according to the color and taking
+/// exceptaions of A and H file to considration
+/// because shiftin that ends up in this files gets
+/// square to one upper rank.
+uint64_t mask_pawn_attacks(int square, int side){
     uint64_t bitboard = 0ull;
     uint64_t mask_bitboard = 0ull;
 
-    set_bit(bitboard, pos);
+    set_bit(bitboard, square);
 
     // white
     if(!side)
@@ -30,13 +32,13 @@ uint64_t mask_pawn_attacks(int pos, int side){
     return mask_bitboard;
 }
 
-/////////////////////////////////////////////////
-// save pre calculate moves????
-/////////////////////////////////////////////////
-// for (int i = 0 ; i < 64; i++){
-    //     printf("\nwhite\n");
-    //     print_bitboard(mask_pawn_attacks(i, white));
+/// initialize pawn attacks for all 64 squares.
+void init_pawn_attacks(){
+    for (int sq = 0; sq < 64; sq++){
+        // white pawn
+        pawn_attacks[WHITE][sq] = mask_pawn_attacks(sq, WHITE);
 
-    //     printf("\nblack\n");
-    //     print_bitboard(mask_pawn_attacks(i, black));
-    // }
+        // black pawn
+        pawn_attacks[BLACK][sq] = mask_pawn_attacks(sq, BLACK);
+    }
+}
--- pawn.h
+++ pawn.h
@@ -5,7 +5,21 @@
 #include "bitboard.h"
 #include "types.h"
 
+extern uint64_t pawn_attacks[2][64];
 
-uint64_t mask_pawn_attacks(int pos, int side);
+/**
+ * @brief masking squares that can be attacked by the specified pawn.
+ * 
+ * @param square 
+ * @param side (black or white)
+ * @return uint64_t bitboard
+ */
+uint64_t mask_pawn_attacks(int square, int side);
+
+/**
+ * @brief initialize pawn attacks for all 64 squares.
+ * 
+ */
+void init_pawn_attacks();
 
 #endif

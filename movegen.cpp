c1970d288777c1c78601cd78acbd3aad0c73f3ef
Babak-SSH <babak.sefidgar@gmail.com>
2022-03-10 22:49:32 +0330

    init movegen files

--- movegen.cpp
+++ movegen.cpp
@@ -1,1 +1,24 @@
 #include "movegen.h"
+
+
+moves* generate_pawn_moves(moves* move_list, Color side){
+
+}
+
+moves* generate_king_moves(moves* move_list){
+
+}
+
+moves* generate_moves(PieceType piece, moves* move_list){
+
+}
+
+void generate_all(moves* move_list){
+    move_list = generate_pawn_moves(move_list);
+    move_list = generate_king_moves(move_list);
+    move_list = generate_moves(KNIGHT, move_list);
+    move_list = generate_moves(BISHOP, move_list);
+    move_list = generate_moves(ROOK, move_list);
+    move_list = generate_moves(QUEEN, move_list);
+}
+
--- movegen.h
+++ movegen.h
@@ -1,6 +1,15 @@
 #ifndef MOVEGEN_H
 #define MOVEGEN_H
 
+#include "bitboard.h"
 
 
+moves* generate_pawn_moves(moves* move_list, Color side);
+
+moves* generate_king_moves(moves* move_list);
+
+moves* generate_moves(PieceType piece, moves* move_list);
+
+void generate_all(moves* move_list);
+  
 #endif

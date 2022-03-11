ed31b4c58126fe31b30621635bcd7585328316bd
Babak-SSH <babak.sefidgar@gmail.com>
2022-03-11 16:49:36 +0330

    add const value of each rank in bitborad

--- cons.h
+++ cons.h
@@ -10,6 +10,15 @@ extern const uint64_t not_H_file;
 extern const uint64_t not_AB_file;
 extern const uint64_t not_GH_file;
 
+extern const uint64_t RANK1;
+extern const uint64_t RANK2;
+extern const uint64_t RANK3;
+extern const uint64_t RANK4;
+extern const uint64_t RANK5;
+extern const uint64_t RANK6;
+extern const uint64_t RANK7;
+extern const uint64_t RANK8;
+
 extern const int bishop_relevant_bits[64];
 extern const int rook_relevant_bits[64];
 

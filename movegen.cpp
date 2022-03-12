45f555050714845e493452de95e540d23c06fb5c
Babak-SSH <babak.sefidgar@gmail.com>
2022-03-12 22:40:48 +0330

    moved encode and decode functions to main file so globally can be used

--- movegen.cpp
+++ movegen.cpp
@@ -214,6 +214,45 @@ void generate_all(moves *move_list, Color side){
     generate_moves(move_list, side, QUEEN);
 }
 
+/*
+          binary move bits                               hexidecimal constants
+    
+    0000 0000 0000 0000 0011 1111    source square       0x3f
+    0000 0000 0000 1111 1100 0000    target square       0xfc0
+    0000 0000 1111 0000 0000 0000    piece               0xf000
+    0000 1111 0000 0000 0000 0000    promoted piece      0xf0000
+    0001 0000 0000 0000 0000 0000    capture flag        0x100000
+    0010 0000 0000 0000 0000 0000    double push flag    0x200000
+    0100 0000 0000 0000 0000 0000    enpassant flag      0x400000
+    1000 0000 0000 0000 0000 0000    castling flag       0x800000
+*/
+/// @todo define structure or fluent interface
+int encode_move(int source, int target, int piece, int promoted, int capture, int double_push, int enpassant, int castling){
+    return ((source) |          \
+    (target << 6) |     \
+    (piece << 12) |     \
+    (promoted << 16) |  \
+    (capture << 20) |   \
+    (double_push << 21) |    \
+    (enpassant << 22) | \
+    (castling << 23));
+}
+
+moveInfo decode_move(int move){
+    moveInfo info;
+
+    info.source = (move & 0x3f);
+    info.target = ((move & 0xfc0) >> 6);
+    info.piece = ((move & 0xf000) >> 12);
+    info.promoted = ((move & 0xf0000) >> 16);
+    info.capture = (move & 0x100000);
+    info.double_push = (move & 0x200000);
+    info.enpassant = (move & 0x400000);
+    info.castling = (move & 0x800000);
+
+    return info;
+}
+
 // printing moves:
 
 /// print move (for UCI purposes)
--- movegen.h
+++ movegen.h
@@ -37,53 +37,18 @@ static char promoted_pieces[] = {
     'n'
 };
 
-/*
-          binary move bits                               hexidecimal constants
-    
-    0000 0000 0000 0000 0011 1111    source square       0x3f
-    0000 0000 0000 1111 1100 0000    target square       0xfc0
-    0000 0000 1111 0000 0000 0000    piece               0xf000
-    0000 1111 0000 0000 0000 0000    promoted piece      0xf0000
-    0001 0000 0000 0000 0000 0000    capture flag        0x100000
-    0010 0000 0000 0000 0000 0000    double push flag    0x200000
-    0100 0000 0000 0000 0000 0000    enpassant flag      0x400000
-    1000 0000 0000 0000 0000 0000    castling flag       0x800000
-*/
-/// @todo define structure or fluent interface
-static inline int encode_move(int source, int target, int piece, int promoted, int capture, int double_push, int enpassant, int castling){
-    return ((source) |          \
-    (target << 6) |     \
-    (piece << 12) |     \
-    (promoted << 16) |  \
-    (capture << 20) |   \
-    (double_push << 21) |    \
-    (enpassant << 22) | \
-    (castling << 23));
-}
+int encode_move(int source, int target, int piece, int promoted, int capture, int double_push, int enpassant, int castling);
 
-static inline moveInfo decode_move(int move){
-    moveInfo info;
+moveInfo decode_move(int move);
 
-    info.source = (move & 0x3f);
-    info.target = ((move & 0xfc0) >> 6);
-    info.piece = ((move & 0xf000) >> 12);
-    info.promoted = ((move & 0xf0000) >> 16);
-    info.capture = (move & 0x100000);
-    info.double_push = (move & 0x200000);
-    info.enpassant = (move & 0x400000);
-    info.castling = (move & 0x800000);
+void print_move(int move);
 
-    return info;
-}
+void print_move_list(moves *move_list);
 
 static inline void add_move(moves *move_list, int move){
     move_list->moves[move_list->count] = move;
     
     move_list->count++;
 }
-
-void print_move(int move);
-
-void print_move_list(moves *move_list);
 
 #endif

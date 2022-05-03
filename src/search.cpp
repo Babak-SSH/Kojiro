da0098208190436ce653605228833347320189f8
Babak-SSH <babak.sefidgar@gmail.com>
2022-05-03 20:25:43 +0430

    sorting moves and scoring capture moves

--- src/search.cpp
+++ src/search.cpp
@@ -1,8 +1,56 @@
+#include <bits/stdc++.h>
+
 #include "search.h"
 
+
 long nodes = 0;
+long nodes2 = 0;
 int ply = 0; // half moves
 int best_move;
+long d = 0;
+int max_ply = 0;
+
+/// scoring moves.
+static int Search::score_move(int move){
+	moveInfo info;
+	int target_piece = 0;
+
+	info = decode_move(move);
+	//score capture moves
+	if(info.capture){
+		for(int bb_piece = B_PAWN-(st->side*6); bb_piece <= B_KING-(st->side*6); bb_piece++){
+			if(get_bit(st->bitboards[bb_piece], info.target)){
+				target_piece = bb_piece;
+				break;
+			}
+		}
+		return mvv_lva[info.piece%6][target_piece%6];
+	}
+	return 0;
+}
+
+/// compare function to compare scores in move,score pair.
+static int Search::compare(std::pair<int, int> t1, std::pair<int, int> t2){
+	return t1.first > t2.first;
+}
+
+/// sorting moves by their respective score evaluated by Search::score_move function.
+static void Search::sort_moves(moves *move_list){
+	int move_count = move_list->count;
+	int move_scores[move_count];
+	std::pair<int, int> ms[move_count];
+
+	for(int count = 0; count < move_count; count++){
+		ms[count].first = Search::score_move(move_list->moves[count]);
+		ms[count].second = move_list->moves[count];
+	}
+
+	std::sort(ms, ms+move_count, Search::compare);
+
+	for(int count = 0; count < move_count; count++){
+		move_list->moves[count] = ms[count].second;
+	}
+}
 
 /// quiescence search is used to avoid the horizon effect. because evaluating in a certain depth 
 /// is dangerous, we need to have not certain but acceptable view on the rest of that position
@@ -10,7 +58,7 @@ int best_move;
 /// without searching all those nodes and to make sure we are only evaluating quiescence (quite) positions.
 static int Search::quiescence(int alpha, int beta){
 	int standpat = Eval::evaluation();
-
+	nodes2++;
 	 /// fail-hard beta cutoff
 	 /// @todo check fail-soft beta cutoff
     if (standpat >= beta){
@@ -28,16 +76,19 @@ static int Search::quiescence(int alpha, int beta){
 
     generate_all(move_list, Color(st->side));
 
-    for (int move_count = 0; move_count < move_list->count; move_count++){
+	Search::sort_moves(move_list);
 
+    for (int move_count = 0; move_count < move_list->count; move_count++){
+		ply++;
 		StateInfo nst;
 		if(!make_move(move_list->moves[move_count], 2, nst)){
+			ply--;
 			continue;
 		}
 
-
 		int score = -Search::quiescence(-beta, -alpha);
 
+		ply--;
 		take_back();
 
 		 /// fail-hard beta cutoff
@@ -81,14 +132,19 @@ static int Search::negamax(int alpha, int beta, int depth){
 
     generate_all(move_list, Color(st->side));
 
+	Search::sort_moves(move_list);
+
     for (int move_count = 0; move_count < move_list->count; move_count++){
 
 		StateInfo nst;
+
+		ply++;
+
 		if(!make_move(move_list->moves[move_count], 1, nst)){
+			ply--;
 			continue;
 		}
 
-		ply++;
 		legal_moves++;
 
 		int score = -negamax(-beta, -alpha, depth-1);
@@ -141,10 +197,17 @@ static int Search::negamax(int alpha, int beta, int depth){
 }
 
 void Search::search(int depth){
+	int score;
 	moveInfo info;
-	Search::negamax(-50000, 50000, depth);
+
+	score = Search::negamax(-50000, 50000, depth);
+
 	if(best_move){
 	info = decode_move(best_move);
+
+	/// @todo this part except bestmove should be logged.
+	sync_cout << "cp: " << Eval::evaluation() << "  score nega: " << score << sync_endl;
+	sync_cout << "negamax nodes: " << nodes << "\nquiescence nodes:" << nodes2 << "\ntotal nodes:" << nodes2+nodes << sync_endl;
 	sync_cout << "bestmove " 
 			  << convert_to_square[info.source] << convert_to_square[info.target] << sync_endl;
 	}
--- src/search.h
+++ src/search.h
@@ -5,9 +5,56 @@
 #include "uci.h"
 #include "eval.h"
 
+// most valuable victim & less valuable attacker
+
+/*
+                          
+    (Victims) Pawn Knight Bishop   Rook  Queen   King
+  (Attackers)
+        Pawn   105    205    305    405    505    605
+      Knight   104    204    304    404    504    604
+      Bishop   103    203    303    403    503    603
+        Rook   102    202    302    402    502    602
+       Queen   101    201    301    401    501    601
+        King   100    200    300    400    500    600
+*/
+
+// MVV LVA [attacker][victim]
+static const int mvv_lva[6][6] ={
+	105, 205, 305, 405, 505, 605,
+	104, 204, 304, 404, 504, 604,
+	103, 203, 303, 403, 503, 603,
+	102, 202, 302, 402, 502, 602,
+	101, 201, 301, 401, 501, 601,
+	100, 200, 300, 400, 500, 600
+};
 
 namespace Search
 {
+	/**
+	 * @brief score moves depending on their type (capture, quite, killer,...)
+	 * 
+	 * @param move 
+	 * @return int 
+	 */
+	static int score_move(int move);
+
+	/**
+	 * @brief compare scores(first) of pair
+	 * 
+	 * @param t1 
+	 * @param t2 
+	 * @return int 
+	 */
+	static int compare(std::pair<int, int> t1, std::pair<int, int> t2);	
+
+	/**
+	 * @brief sorting moves according to their score.
+	 * 
+	 * @param move_list 
+	 */
+	static void sort_moves(moves *move_list);
+
 	/**
 	 * @brief quiescence search for horizon effect.
 	 * 

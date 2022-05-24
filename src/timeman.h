c493946e5b62b95da92ccb2313149e3fce52f699
Babak-SSH <babak.sefidgar@gmail.com>
2022-05-24 16:21:23 +0430

    Add: TimePoint and TimeManagement class

--- src/timeman.h
+++ src/timeman.h
@@ -1,4 +1,32 @@
 #ifndef TIMEMAN_H
 #define TIMEMAN_H
 
-#endif
+#include <chrono>
+
+namespace Kojiro {
+
+typedef std::chrono::milliseconds::rep TimePoint; // A value in milliseconds
+static_assert(sizeof(TimePoint) == sizeof(int64_t), "TimePoint should be 64 bits");
+inline TimePoint now() {
+  return std::chrono::duration_cast<std::chrono::milliseconds>
+        (std::chrono::steady_clock::now().time_since_epoch()).count();
+}
+
+class TimeManagement {
+public:
+	void init(int side);
+	TimePoint getOptimum() const { return optimumTime; };
+	TimePoint getMaximum() const { return maximumTime; };
+	TimePoint getElapsed() const { return now() - startTime; };
+
+private:
+	TimePoint startTime;
+	TimePoint optimumTime;
+	TimePoint maximumTime;
+};
+
+extern TimeManagement Time;
+
+} // namespace Kojiro
+
+#endif // #ifndef TIMEMAN_H

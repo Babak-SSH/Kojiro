97ecabefdf8ff503cccfd31e0f40295bd716f267
Babak-SSH <babak.sefidgar@gmail.com>
2022-05-25 17:16:30 +0430

    Add: a simple function for time management(needs improvement)

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -1,1 +1,21 @@
-#include "time.h"
+#include "timeman.h"
+#include "search.h"
+
+
+namespace Kojiro {
+	TimeManagement Time; // main TimeMangement object
+
+	void TimeManagement::init(Search::GameInfo& info, Color side, int ply) {
+		startTime = info.startTime;
+		int moveOverHead = 10;
+		int remainingMoves = 50;	
+		double optScale;
+
+		TimePoint timeLeft = std::max(TimePoint(1), info.time[side] + (info.inc[side] * (remainingMoves-1)) - (moveOverHead * remainingMoves));
+		optScale = std::min((0.88 + ply / 116.4) / remainingMoves,
+                            0.88 * info.time[st->side] / double(timeLeft));
+
+		optimumTime = TimePoint(optScale * timeLeft);
+		maximumTime = TimePoint(timeLeft);
+	}
+}
--- src/timeman.h
+++ src/timeman.h
@@ -1,20 +1,15 @@
 #ifndef TIMEMAN_H
 #define TIMEMAN_H
 
-#include <chrono>
+#include "search.h"
+#include "misc.h"
+
 
 namespace Kojiro {
 
-typedef std::chrono::milliseconds::rep TimePoint; // A value in milliseconds
-static_assert(sizeof(TimePoint) == sizeof(int64_t), "TimePoint should be 64 bits");
-inline TimePoint now() {
-  return std::chrono::duration_cast<std::chrono::milliseconds>
-        (std::chrono::steady_clock::now().time_since_epoch()).count();
-}
-
 class TimeManagement {
 public:
-	void init(int side);
+	void init(Search::GameInfo& info, Color side, int ply);
 	TimePoint getOptimum() const { return optimumTime; };
 	TimePoint getMaximum() const { return maximumTime; };
 	TimePoint getElapsed() const { return now() - startTime; };

3c59ac9aadee0e77d7b1e84005738d3933cd6887
Babak-SSH <babak.sefidgar@gmail.com>
2022-06-10 17:01:59 +0430

    Add: functions to populate and reset transposition table

--- src/tt.cpp
+++ src/tt.cpp
@@ -1,1 +1,60 @@
 #include "tt.h"
+#include "position.h"
+
+
+namespace Kojiro {
+
+TTEntry tt[hash_size];
+
+void TTEntry::reset() {
+	key = 0;
+	depth = 0;
+	flag = 0;
+	score = 0;
+}
+
+/// read transposition table entries data and find the proper score or return no_hash_entry
+int probe_hash(int alpha, int beta, int depth) {
+    TTEntry *hash_entry = &tt[st->key % hash_size];
+    
+    // check position hash keys
+    if (hash_entry->key == st->key)
+    {
+        // we don't want to use tt if the depth is smaller than our current depth
+        if (hash_entry->depth >= depth)
+        {
+            // (PV node) score 
+            if (hash_entry->flag == hashfEXACT)
+                return hash_entry->score;
+            
+            // alpha (fail-low node) score
+            if ((hash_entry->flag == hashfALPHA) && (hash_entry->score <= alpha))
+                return alpha;
+            
+            // beta (fail-high node) score
+            if ((hash_entry->flag == hashfBETA) && (hash_entry->score >= beta))
+                return beta;
+        }
+    }
+    
+    return no_hash_entry;
+}
+
+/// write to transposition table entries
+void write_hash(int score, int depth, int hash_flag) {
+    TTEntry *hash_entry = &tt[st->key % hash_size];
+  
+    hash_entry->key = st->key;
+    hash_entry->score = score;
+    hash_entry->flag = hash_flag;
+    hash_entry->depth = depth;
+}
+
+/// reset all items in our transposition table
+void clear_tt() {
+	for(int i=0; i<hash_size; i++) {
+		tt[i].reset();
+	}
+}
+
+} // namespace Kojiro
--- src/tt.h
+++ src/tt.h
@@ -1,4 +1,54 @@
 #ifndef TT_H
 #define TT_H
 
+#include <string>
+
+#define	hash_size 0x400000
+#define	no_hash_entry 100000
+
+#define    hashfEXACT   0
+#define    hashfALPHA   1
+#define    hashfBETA    2
+
+
+namespace Kojiro {
+
+typedef struct TTEntry
+{
+	uint64_t key;
+	int depth;
+	int flag;
+	int score;
+	void reset();
+};
+
+/**
+ * @brief Read hash entries in transposition table.
+ * 
+ * @param alpha 
+ * @param beta 
+ * @param depth 
+ * @return int 
+ */
+int probe_hash(int alpha, int beta, int depth);
+
+/**
+ * @brief Write hash entries in transposition table.
+ * 
+ * @param score 
+ * @param depth 
+ * @param hash_flag 
+ */
+void write_hash(int score, int depth, int hash_flag);
+
+/**
+ * @brief Reset all entries in transposition table.
+ * 
+ */
+void clear_tt();
+
+extern TTEntry tt[hash_size];
+
+} // namespace Kojiro
+
 #endif

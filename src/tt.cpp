f000267cd72de533659c25b775a37fd66757d12e
Babak-SSH <babak.sefidgar@gmail.com>
2022-09-18 15:34:15 +0430

    Modify: change(reversed) logic on retrieving score from pv path

--- src/tt.cpp
+++ src/tt.cpp
@@ -1,5 +1,6 @@
 #include "tt.h"
 #include "position.h"
+#include "eval.h"
 
 
 namespace Kojiro {
@@ -24,16 +25,23 @@ int probe_hash(int alpha, int beta, int depth) {
         // we don't want to use tt if the depth is smaller than our current depth
         if (hash_entry->depth >= depth)
         {
+            int score = hash_entry->score;
+
+            // retrieve score independent from the actual path
+            // from root node (position) to current node (position)
+            if (score < -mate_score) score += st->play_count;
+            if (score > mate_score) score -= st->play_count;
+
             // (PV node) score 
             if (hash_entry->flag == hashfEXACT)
-                return hash_entry->score;
+                return score;
             
             // alpha (fail-low node) score
-            if ((hash_entry->flag == hashfALPHA) && (hash_entry->score <= alpha))
+            if ((hash_entry->flag == hashfALPHA) && (score <= alpha))
                 return alpha;
             
             // beta (fail-high node) score
-            if ((hash_entry->flag == hashfBETA) && (hash_entry->score >= beta))
+            if ((hash_entry->flag == hashfBETA) && (score >= beta))
                 return beta;
         }
     }
@@ -44,7 +52,10 @@ int probe_hash(int alpha, int beta, int depth) {
 /// write to transposition table entries
 void write_hash(int score, int depth, int hash_flag, int move) {
     TTEntry *hash_entry = &tt[st->key % hash_size];
-  
+
+    if (score < -mate_score) score -= play_count;
+    if (score > mate_score) score += play_count;
+
     hash_entry->key = st->key;
     hash_entry->score = score;
     hash_entry->flag = hash_flag;

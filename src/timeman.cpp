63fd0c97f60989a67f9a0ad3ba029f03c5e632de
Babak-SSH <babak.sefidgar@gmail.com>
2022-12-05 14:40:48 +0330

    Modify: remove move limit(50) timing and replace it with a simplified graph of time taken from stockfish

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -1,6 +1,8 @@
 #include "timeman.h"
 #include "search.h"
 
+#include <cmath>
+
 
 namespace Kojiro {
 	TimeManagement Time; // main TimeMangement object
@@ -12,8 +14,13 @@ namespace Kojiro {
 		double optScale;
 
 		TimePoint timeLeft = std::max(TimePoint(1), info.time[side] + (info.inc[side] * (remainingMoves-1)) - (moveOverHead * remainingMoves));
-		optScale = std::min((0.88 + ply / 116.4) / remainingMoves,
-                            0.88 * info.time[st->side] / double(timeLeft));
+		
+		optScale = std::min(0.0088 + std::pow(ply + 3.0, 0.5) * 0.0044,
+							0.88 * info.time[st->side] / double(timeLeft));
+
+		// for move limit
+		// optScale = std::min((0.88 + ply / 116.4) / remainingMoves,
+                            // 0.88 * info.time[st->side] / double(timeLeft));
 
 		optimumTime = TimePoint(optScale * timeLeft);
 		maximumTime = TimePoint(timeLeft);

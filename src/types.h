17f5dd671a47bf8dddec2c9e88fe9c4526439853
Babak-SSH <babak.sefidgar@gmail.com>
2022-12-09 22:34:52 +0330

    Add: number of enums as element in enum

--- src/types.h
+++ src/types.h
@@ -3,21 +3,29 @@
 
 // names representing each square in chess board 
 // according to their file and rank
-enum square {a8,b8,c8,d8,e8,f8,g8,h8,
-             a7,b7,c7,d7,e7,f7,g7,h7,
-             a6,b6,c6,d6,e6,f6,g6,h6,
-             a5,b5,c5,d5,e5,f5,g5,h5,
-             a4,b4,c4,d4,e4,f4,g4,h4,
-             a3,b3,c3,d3,e3,f3,g3,h3,
-             a2,b2,c2,d2,e2,f2,g2,h2,
-             a1,b1,c1,d1,e1,f1,g1,h1,no_sq};
+enum square : int {
+  a8,b8,c8,d8,e8,f8,g8,h8,
+  a7,b7,c7,d7,e7,f7,g7,h7,
+  a6,b6,c6,d6,e6,f6,g6,h6,
+  a5,b5,c5,d5,e5,f5,g5,h5,
+  a4,b4,c4,d4,e4,f4,g4,h4,
+  a3,b3,c3,d3,e3,f3,g3,h3,
+  a2,b2,c2,d2,e2,f2,g2,h2,
+  a1,b1,c1,d1,e1,f1,g1,h1,
+  no_sq,
+  
+  SQUARE_NB = 64
+};
 
 enum Color {WHITE, BLACK, NO_COLOR};
 
 enum PieceType {PAWN, KNIGHT, BISHOP,  ROOK, QUEEN, KING};
 
-enum Piece {W_PAWN, W_KNIGHT, W_BISHOP, W_ROOK, W_QUEEN, W_KING,
-            B_PAWN, B_KNIGHT, B_BISHOP, B_ROOK, B_QUEEN, B_KING};
+enum Piece {
+  W_PAWN, W_KNIGHT, W_BISHOP, W_ROOK, W_QUEEN, W_KING,
+  B_PAWN, B_KNIGHT, B_BISHOP, B_ROOK, B_QUEEN, B_KING,
+  PIECE_NB = 16
+};
 
 // encode pieces
 enum { P, N, B, R, Q, K, p, n, b, r, q, k };

c75059080b4ea3665fba9f4a1115ee76e08e8912
Babak-SSH <babak.sefidgar@gmail.com>
2023-01-31 00:55:42 +0330

    --amend

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -1,4 +1,5 @@
 #include "movegen.h"
+#include "bitboard.h"
 
 
 namespace Kojiro {
@@ -15,31 +16,25 @@ void generate_pawn_moves(moves *move_list, Color side){
     promotion_rank = (side ? RANK2 : RANK7);
     double_push = (side ? RANK7 : RANK2); 
 
-    while (bb)
-    {
+    while (bb) {
         source_sq = get_ls1b_index(bb);
         target_sq = (side ? source_sq + 8 : source_sq - 8);
 
         // generate quite pawn moves
-        if (!(target_sq < a8) && !get_bit(st->occupancies[NO_COLOR], target_sq))
-        {
+        if (!(target_sq < a8) && !get_bit(st->occupancies[NO_COLOR], target_sq)) {
             // pawn promotion
-            if ((1ull << source_sq) & promotion_rank)
-            {
+            if ((1ull << source_sq) & promotion_rank) {
                 add_move(move_list, encode_move(source_sq, target_sq, piece, Q, 0, 0, 0, 0));
                 add_move(move_list, encode_move(source_sq, target_sq, piece, R, 0, 0, 0, 0));
                 add_move(move_list, encode_move(source_sq, target_sq, piece, B, 0, 0, 0, 0));
                 add_move(move_list, encode_move(source_sq, target_sq, piece, N, 0, 0, 0, 0));
             }
-            
-            else
-            {
+            else {
                 // one square ahead pawn move
                 add_move(move_list, encode_move(source_sq, target_sq, piece, 0, 0, 0, 0, 0));
 
                 // two squares ahead pawn move
-                if (((1ull << source_sq) & double_push) && !get_bit(st->occupancies[NO_COLOR], (side ? target_sq + 8 : target_sq - 8)))
-                {
+                if (((1ull << source_sq) & double_push) && !get_bit(st->occupancies[NO_COLOR], (side ? target_sq + 8 : target_sq - 8))) {
                     add_move(move_list, encode_move(source_sq, (side ? target_sq + 8 : target_sq - 8), piece, 0, 0, 1, 0, 0));
                 }
             }
@@ -49,22 +44,18 @@ void generate_pawn_moves(moves *move_list, Color side){
         attacks = pawn_attacks[side][source_sq] & st->occupancies[!side];
         
         // generate pawn captures
-        while (attacks)
-        {
+        while (attacks) {
             // init target square
             target_sq = get_ls1b_index(attacks);
             
             // pawn promotion
-            if ((1ull << source_sq) & promotion_rank)
-            {
+            if ((1ull << source_sq) & promotion_rank) {
                 add_move(move_list, encode_move(source_sq, target_sq, piece, Q, 1, 0, 0, 0));
                 add_move(move_list, encode_move(source_sq, target_sq, piece, R, 1, 0, 0, 0));
                 add_move(move_list, encode_move(source_sq, target_sq, piece, B, 1, 0, 0, 0));
                 add_move(move_list, encode_move(source_sq, target_sq, piece, N, 1, 0, 0, 0));
             }
-            
-            else
-            {
+            else {
                 // one square ahead pawn move
                 add_move(move_list, encode_move(source_sq, target_sq, piece, 0, 1, 0, 0, 0));
             }
@@ -73,14 +64,12 @@ void generate_pawn_moves(moves *move_list, Color side){
         }
         
         // generate enpassant captures
-        if (st->enpassant != no_sq)
-        {
+        if (st->enpassant != no_sq) {
             // lookup pawn attacks and bitwise AND with enpassant square (bit)
             uint64_t enpassant_attacks = pawn_attacks[side][source_sq] & (1ULL << st->enpassant);
             
             // make sure enpassant capture available
-            if (enpassant_attacks)
-            {
+            if (enpassant_attacks) {
                 // init enpassant capture target square
                 int target_enpassant = get_ls1b_index(enpassant_attacks);
                 add_move(move_list, encode_move(source_sq, target_enpassant, piece, 0, 1, 0, 1, 0));
@@ -101,8 +90,7 @@ void generate_king_moves(moves *move_list, Color side){
     int ksq = (side ? e8 : e1);
     bb = st->bitboards[K+(side*6)];
 
-    while (bb)
-    {
+    while (bb) {
         // init source square
         source_sq = get_ls1b_index(bb);
         
@@ -110,8 +98,7 @@ void generate_king_moves(moves *move_list, Color side){
         attacks = king_attacks[source_sq] & ~(st->occupancies[side]);
         
         // loop over target squares available from generated attacks
-        while (attacks)
-        {
+        while (attacks) {
             // init target square
             target_sq = get_ls1b_index(attacks);    
             
@@ -132,13 +119,11 @@ void generate_king_moves(moves *move_list, Color side){
     }
 
      // king side castling is available
-    if (st->castle & (side ? BK : WK))
-    {
+    if (st->castle & (side ? BK : WK)) {
         int sq1 = (side ? f8 : f1);
         int sq2 = (side ? g8 : g1);
 
-        if (!get_bit(st->occupancies[NO_COLOR], sq1) && !get_bit(st->occupancies[NO_COLOR], sq2))
-        {
+        if (!get_bit(st->occupancies[NO_COLOR], sq1) && !get_bit(st->occupancies[NO_COLOR], sq2)) {
             if (!is_square_attacked(ksq, !side) && !is_square_attacked(sq1, !side))
                 add_move(move_list, encode_move(ksq, sq2, piece, 0, 0, 0, 0, 1));
                 /// @todo check consistancy for all conditions (complete rework on king moves)
@@ -147,15 +132,13 @@ void generate_king_moves(moves *move_list, Color side){
     }
     
     // queen side castling is available
-    if (st->castle & (side ? BQ : WQ))
-    {
+    if (st->castle & (side ? BQ : WQ)) {
         int sq1 = (side ? d8 : d1);
         int sq2 = (side ? c8 : c1);
         int sq3 = (side ? b8 : b1);
 
         // make sure square between king and queen's rook are empty
-        if (!get_bit(st->occupancies[NO_COLOR], sq1) && !get_bit(st->occupancies[NO_COLOR], sq2) && !get_bit(st->occupancies[NO_COLOR], sq3))
-        {
+        if (!get_bit(st->occupancies[NO_COLOR], sq1) && !get_bit(st->occupancies[NO_COLOR], sq2) && !get_bit(st->occupancies[NO_COLOR], sq3)) {
             // make sure king and the d1 squares are not under attacks
             if (!is_square_attacked(ksq, !side) && !is_square_attacked(sq1, !side))
                 add_move(move_list, encode_move(ksq, sq2, piece, 0, 0, 0, 0, 1));
@@ -165,15 +148,14 @@ void generate_king_moves(moves *move_list, Color side){
 
 /// @todo make a template function for all pieces or find a better way
 /// generate all legal moves for sliding pieces and knights.
-void generate_moves(moves *move_list, Color side, PieceType pType){
+void generate_moves(moves *move_list, Color side, PieceType pType) {
     int source_sq, target_sq;
     uint64_t bb, attacks;
 
     Piece piece = (side ? (Piece(pType+side*6)) : Piece(pType));
     bb = st->bitboards[piece];
 
-    while (bb)
-    {
+    while (bb) {
         // init source square
         source_sq = get_ls1b_index(bb);
         
@@ -188,8 +170,7 @@ void generate_moves(moves *move_list, Color side, PieceType pType){
             attacks = get_queen_attacks(source_sq, st->occupancies[NO_COLOR]) & ~st->occupancies[side];
         
         // loop over target squares available from generated attacks
-        while (attacks)
-        {
+        while (attacks) {
             // init target square
             target_sq = get_ls1b_index(attacks);    
             

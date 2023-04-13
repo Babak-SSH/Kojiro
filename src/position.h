99bf235cb18ab237feb9d3606fdc07ab6128e9d5
Babak-SSH <babak.sefidgar@gmail.com>
2023-04-13 14:02:27 +0330

    Add: add Position class so we can have both stateinfo and additional info about the game for each thread seperatly and most importantly get rid off global state which we use for single thread form of the engine. Modify: moved most of the variables and functions which were related to Position to Position Class.

--- src/position.h
+++ src/position.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <cstring>
+#include <sys/types.h>
 
 #include "cons.h"
 #include "bitboard.h"
@@ -40,9 +41,156 @@ struct StateInfo {
     StateInfo* previous;
 };
 
+// forward declaring Thread class to avoid including recursively
+class Thread;
 
-// temp state info
-extern StateInfo* st;
+class Position {
+
+    private:
+        Thread* thisTh;
+        StateInfo* st;
+
+    public:
+        /**
+         * @brief Initialize hash keys.
+         * 
+         */
+        static void init_hash();
+
+        Position() = default;
+        Position(const Position&) = delete;
+
+        /**
+         * @brief parse fen and set the variables.
+         * 
+         * @param fen 
+         * @param state 
+         * @param thread 
+         * @return Position& 
+         */
+        Position& parse_fen(const std::string& fen, StateInfo* st, Thread* th);
+
+        /**
+         * @brief Get the fen string
+         * 
+         * @return std::string 
+         */
+        std::string get_fen();
+
+        /**
+         * @brief return an ASCII representation of the position.
+         * 
+         */
+        void log_board();
+
+        /**
+         * @brief generate hash key.
+         * 
+         * @return uint64_t 
+         */
+        uint64_t generate_hash_key();
+
+        /**
+         * @brief take state back.
+         * 
+         */
+        void take_back();
+
+        /**
+         * @brief check if side that will play is in check
+         * 
+         * @return true if the side that should play is in check
+         * @return false if the side that should play is not in check 
+         */
+        bool is_check();
+
+        /**
+         * @brief do the move on that state.
+         * 
+         * @param move 
+         * @param move_flag 
+         * @param newST 
+         * @return int 
+         */
+        int make_move(int move, int move_flag, StateInfo& newST, int depth=-1);
+
+        /**
+         * @brief Set the piece in given square on piece bitboard. 
+         * 
+         * @param p 
+         * @param sq 
+         */
+        void set_piece(Piece p, int sq);
+        
+        /**
+         * @brief  check if square is under attack.
+         * 
+         * @param square 
+         * @param side 
+         * @return true 
+         * @return false 
+         */
+        bool is_square_attacked(int square, int side) const;
+
+        int enpassant() const;
+        int side() const;
+        int castle() const;
+        int play_count() const;
+        u_int64_t bitboards(int p) const;
+        u_int64_t occupancies(int color) const;
+        u_int64_t key() const;
+        StateInfo* state() const;
+};
+
+inline int Position::enpassant() const {
+    return st->enpassant;
+}
+
+inline int Position::side() const {
+    return st->side;
+}
+
+inline int Position::castle() const {
+    return st->castle;
+}
+
+inline int Position::play_count() const {
+    return st->play_count;
+}
+
+inline u_int64_t Position::bitboards(int p) const {
+    return st->bitboards[p];
+}
+
+inline u_int64_t Position::occupancies(int color) const {
+    return st->occupancies[color];
+}
+
+inline u_int64_t Position::key() const {
+    return st->key;
+}
+
+inline StateInfo* Position::state() const {
+    return st;
+}
+
+/// @todo get_attacks function with piece parameter to get rid of army of ifs.(maybe template)
+inline bool Position::is_square_attacked(int square, int side) const {
+    // pawn
+    if (pawn_attacks[!side][square] & st->bitboards[P+(6*side)]) return 1;
+    // knight
+    if (knight_attacks[square] & st->bitboards[N+(6*side)]) return 1;
+    // bishop
+    if (get_bishop_attacks(square, st->occupancies[NO_COLOR]) & st->bitboards[B+(6*side)]) return 1;
+    // rook
+    if (get_rook_attacks(square, st->occupancies[NO_COLOR]) & st->bitboards[R+(6*side)]) return 1;
+    // queen
+    if (get_queen_attacks(square, st->occupancies[NO_COLOR]) & st->bitboards[Q+(6*side)]) return 1;
+    // king
+    if (king_attacks[square] & st->bitboards[K+(6*side)]) return 1;
+
+    return 0;
+}
 
 // piece bitboards
 extern uint64_t bitboards[12];
@@ -63,92 +211,11 @@ extern int rule50;
 
 extern int play_count;
 
-/**
- * @brief return an ASCII representation of the position.
- * 
- */
-void log_board();
-
-/**
- * @brief generate hash key.
- * 
- * @return uint64_t 
- */
-uint64_t generate_hash_key();
-
-/**
- * @brief Initialize hash keys.
- * 
- */
-void init_hash();
-
-/**
- * @brief Basic set bit instructions to build board.
- * 
- */
-void init_start();
-
-/**
- * @brief Initialize state variables.
- * 
- */
-void init_state();
-
-/**
- * @brief take state back.
- * 
- */
-void take_back();
-
-/**
- * @brief Get the fen string
- * 
- * @return std::string 
- */
-std::string get_fen();
-
-/**
- * @brief parse fen and assign to the variables.
- * 
- * @param fen 
- */
-void parse_fen(const std::string& fen);
-
-/**
- * @brief check if side that will play is in check
- * 
- * @return true if the side that should play is in check
- * @return false if the side that should play is not in check 
- */
-bool is_check();
-
-/**
- * @brief  check if square is under attack.
- * 
- * @param square 
- * @param side 
- * @return true 
- * @return false 
- */
-bool is_square_attacked(int square, int side);
-
-/// @todo enhance color(side) finding
-inline void set_piece(Piece p, int sq){
-    set_bit(st->bitboards[p], sq);
-    
-    st->occupancies[(p > 5)] |= st->bitboards[p];
-    st->occupancies[2] |= st->bitboards[p];
-}
-
-/**
- * @brief do the move on that state.
- * 
- * @param move 
- * @param move_flag 
- * @param newST 
- * @return int 
- */
-int make_move(int move, int move_flag, StateInfo& newST, int depth=-1);
+// /**
+//  * @brief Initialize state variables.
+//  * 
+//  */
+// void init_state();
 
 extern long captures_count;
 extern long captures_flag;
